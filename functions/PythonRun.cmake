93ed75709198c9f911ecb72f2209cd60d97ee458
Andres Gongora <mail@andresgongora.com>
2023-05-27 13:17:17 +0200

    Add PythonRun.cmake placeholder
    Signed-off-by: Andres Gongora <mail@andresgongora.com>

---
+++ functions/PythonRun.cmake
@@ -0,0 +1,19 @@
+####################################################################################################
+##
+##  PYTHON RUN
+##
+##  To do:
+##      See functions: https://hsf-training.github.io/hsf-training-cmake-webpage/11-functions/index.html
+##      - Create a function to check if all is ready (eg: is python ready to run, is pip accesible): https://github.com/mavlink/mavlink/blob/master/CMakeLists.txt
+##      - Create a function that accepts a python script and runs it (it first checks if everything is ready)
+##      - Create a function that accepts requirements.txt and isntalls it with pip (it first checks if everything is ready)
+##      - A script or function that lets the user run a specific python command.
+##      - Option to determine when the script/function is run: on every CMake, on every Make, when a specifi
+##        list of watched files change+
+##      - Multiple checks to make sure python is available
+##      - Maybe can be extended to a generic "external command run", the specialized for python
+##
+####################################################################################################
+
+
+
